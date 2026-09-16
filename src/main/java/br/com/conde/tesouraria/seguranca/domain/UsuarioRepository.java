8f5c9d91d3edbdc86b94a724840d96cdcac39ceb
condeDeveloper <183445812+condeDeveloper@users.noreply.github.com>
2026-09-15 21:35:43 -0300

    Repositório de usuários

---
+++ src/main/java/br/com/conde/tesouraria/seguranca/domain/UsuarioRepository.java
@@ -0,0 +1,13 @@
+package br.com.conde.tesouraria.seguranca.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, UUID> {
+
+    Optional<Usuario> findByLogin(String login);
+
+    boolean existsByLogin(String login);
+}
